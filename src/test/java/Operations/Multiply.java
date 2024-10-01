6d136940f289c43cc3112eef883418e2b89341f7
shirishaNeelamsetti <shirishaneelamsetti04@gmail,com>
2024-10-01 13:13:33 +0530

    i added one mutiply class in current project

---
+++ src/test/java/Operations/Multiply.java
@@ -0,0 +1,14 @@
+package Operations;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 20;
+		int c = a * b;
+		System.out.println("the mutiply of a and b values are:" + c);
+
+	}
+
+}
