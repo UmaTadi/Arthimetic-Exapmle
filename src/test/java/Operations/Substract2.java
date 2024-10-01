954d131660b69d1042a4a108ac170ce8e24a0083
Machendergoud <gangapurammachender785@gmail.com>
2024-10-01 14:19:19 +0530

    This is my Second commit

---
+++ src/test/java/Operations/Substract2.java
@@ -0,0 +1,15 @@
+package Operations;
+
+public class Substract2 {
+
+	public static void main(String[] args) {
+
+
+		int a= 200;
+		int b= 15;
+		int c=a-b;
+		System.out.println(c);
+
+	}
+
+}
---
+++ src/test/java/Operations/Substract3.java
@@ -0,0 +1,15 @@
+package Operations;
+
+public class Substract3 {
+
+	public static void main(String[] args) {
+
+
+		int a= 20;
+		int b= 150;
+		int c=a-b;
+		System.out.println(c);
+
+	}
+
+}
