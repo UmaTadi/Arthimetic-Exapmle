0b8ee45e025bcb6ca17ed7c5ed6ee2054620efb9
Jprao99 <preethamjonpelly@gmail.com>
2024-10-01 12:11:36 +0530

    Adding the add class

---
+++ src/test/java/Operations/Add.java
@@ -0,0 +1,12 @@
+package Operations;
+
+public class Add {
+	
+	public static void main(String[] args) {
+		int a=23;
+		int b=45;
+		int c=a+b;
+		System.out.println("Addition of two numbers="+c);
+	}
+
+}
