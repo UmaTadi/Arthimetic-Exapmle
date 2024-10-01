00c561ba897c76ae3b2eb2e42e7d6d70329778e4
UmaTadi <uma.tadi1997@gmail.com>
2024-10-01 11:56:56 +0530

    first commit

---
+++ src/test/java/Operations/Division.java
@@ -0,0 +1,12 @@
+package Operations;
+
+public class Division 
+{
+public static void main(String[] args) 
+{
+	int a=10;
+	int b=2;
+	int result= a/b;
+	System.out.println("Division of both numbers ="+ result);
+}
+}
